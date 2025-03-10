cc63b3c59d32bf0720adbb0c0f1f9cc538d92e12
ONSEMI\zbtwxg <muhammadaididbin.roslan@onsemi.com>
2025-03-10 18:46:11 +0800

    Update SQL scripts

--- src/main/resources/sql/insert.sql
+++ src/main/resources/sql/insert.sql
@@ -1,11 +1,12 @@
 -- INSERT SAMPLE DATA
 INSERT INTO Customer (first_name, last_name, email_office, email_personal, family_members)
 VALUES 
-('John', 'Doe', 'john.doe@office.com', 'john.doe@gmail.com', 'Jane Doe, Jim Doe'),
-('Alice', 'Smith', 'alice.smith@office.com', 'alice.smith@gmail.com', 'Bob Smith, Charlie Smith');
+('Ali', 'Selasa', 'ali.selasa@office.com', 'ali.selasa@gmail.com', 'Ahad Selasa, Zainab Selasa'),
+('Abu', 'Syafiq', 'abu.syafiq@office.com', 'abu.syafiq@gmail.com', 'Embun Syafiq, Ramdan Syafiq');
 
 INSERT INTO Product (book_title, book_price, book_quantity)
 VALUES 
-('Spring Boot in Action', 29.99, 100),
-('Java Concurrency in Practice', 39.99, 50),
-('Effective Java', 45.99, 75);
+('Cloud 101', 29.99, 100),
+('Learn more about the cloud before saying anything!', 11.99, 30),
+('Cloud Best Practices', 39.99, 50),
+('On-Prem Cloud?', 45.99, 75);
